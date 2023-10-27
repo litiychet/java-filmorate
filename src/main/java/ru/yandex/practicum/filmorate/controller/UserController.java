1fde2275f37498b9518d7bfb8541e9ce45f8f69a
Ilya Vetrenko <litiychet@yandex.ru>
2023-10-27 21:27:58 +1000

    fix: checkstyle fix

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -7,7 +7,6 @@ import ru.yandex.practicum.filmorate.model.Marker;
 import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
