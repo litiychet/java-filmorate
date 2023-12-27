4ab67f327b06bc644f4e5224d97e218380c87ce5
Ilya Vetrenko <litiychet@yandex.ru>
2023-12-27 23:31:39 +1000

    fix: checkstyle fix

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -5,11 +5,8 @@ import lombok.Singular;
 
 import javax.validation.constraints.*;
 import java.time.LocalDate;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
-
 
 @Data
 public class Film {
